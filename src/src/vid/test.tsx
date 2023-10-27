fb89fda6f71095fea9471a08751cacdd10b4803f
Veerin Phana-ngam <beam7684@gmail.com>
2023-10-27 13:51:10 +0700

    aaaa

--- src/src/vid/test.tsx
+++ src/src/vid/test.tsx
@@ -1,6 +1,12 @@
 function Vid() {
   return (
     <div>
+      {/* <video
+        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        autoPlay
+        controls
+        playsInline
+      /> */}
       <video
         src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
         autoPlay
