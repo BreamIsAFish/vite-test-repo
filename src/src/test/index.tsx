fc53d897969308441c04637602ed2d92c08ea48d
Veerin Phana-ngam <beam7684@gmail.com>
2023-11-29 11:24:26 +0700

    tag-1

--- src/src/test/index.tsx
+++ src/src/test/index.tsx
@@ -1,19 +1,20 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Test() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  console.log("Test 1");
 
   useEffect(() => {
-    return () => console.log('cleaned up')
-  }, [])
+    return () => console.log("cleaned up");
+  }, []);
 
   const a = () => {
-    navigate('/vid')
-  }
-  return <span onClick={a}>next</span>
+    navigate("/vid");
+  };
+  return <span onClick={a}>next</span>;
   // return <Link to="/1">Next</Link>;
   // return <Link to="/1">Next</Link>;
 }
 
-export default Test
+export default Test;
