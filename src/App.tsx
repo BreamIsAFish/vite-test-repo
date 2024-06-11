4e759d81d1576a35e435735cfbcad1fe87e08d53
Veerin Phana-ngam <beam7684@gmail.com>
2024-06-11 18:43:34 +0700

    feat: Nice 2 feature
    Description ...
    
    BREAKING CHANGE: This is breaking change

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
   console.log('Big 3')
   console.log('Big 4')
   console.log('Big 5')
+  console.log('Big +')
   // const [count, setCount] = useState(0)
   // const [count, setCount] = useState(0)
 
