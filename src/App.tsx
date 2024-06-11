7720d9b5b3ea66c294d39a51667e1c2ac1ea1234
Veerin Phana-ngam <beam7684@gmail.com>
2024-06-11 19:09:02 +0700

    fix: test-2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Vid from './src/vid/test'
 function App() {
   console.log('test 4')
   console.log('test 3')
-  console.log('Big 2')
 
   // const [count, setCount] = useState(0)
   // const [count, setCount] = useState(0)
