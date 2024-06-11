0c88747006e49e19b061d7a77b033322086e70c3
Beam <beam7684@gmail.com>
2024-06-11 18:54:53 +0700

    feat: Test-feat
    fix: WTF
    fix: WTF 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App() {
   console.log('test 3')
   console.log('Big 2')
   console.log('Big 3')
-  console.log('Big 4')
 
   // const [count, setCount] = useState(0)
   // const [count, setCount] = useState(0)
